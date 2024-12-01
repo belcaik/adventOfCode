3f199095416d6a2e7f35bf41b9605198d9e4f12f
belcaik <belcaik@gmail.com>
2024-12-01 17:37:31 -0300

    day 1 part 1

---
+++ src/day1/day1_1.ts
@@ -0,0 +1,33 @@
+import { readFileSync } from 'fs';
+
+console.time('executionTime');
+
+const inputPath: string = __dirname + '/input.txt';
+const input = readFileSync(inputPath, 'utf-8');
+const groupOne: number[] = [];
+const groupTwo: number[] = [];
+const differences: number[] = [];
+const rows = input.split('\n');
+const rowsLength = rows.length;
+
+rows.forEach((row) => {
+    const splittedRow = row.split(' ');
+    let aux: number;
+    aux = parseInt(splittedRow[0]);
+    groupOne.push(aux);
+    aux = parseInt(splittedRow[1]);
+    groupTwo.push(aux);
+});
+
+groupOne.sort((a: number, b: number) => a - b);
+groupTwo.sort((a: number, b: number) => a - b);
+
+for (let i = 0; i < rowsLength; i++) {
+    let absDiff = Math.abs(groupOne[i] - groupTwo[i]);
+    differences.push(absDiff);
+}
+
+const sum = differences.reduce((a, b) => a + b, 0);
+
+console.log(`La suma de las diferencias es ${sum}`);
+console.timeEnd('executionTime');
