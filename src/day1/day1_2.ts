bc08a96e9aea5a0d61bcebc0e4fbb40c960752fc
belcaik <belcaik@gmail.com>
2024-12-01 18:04:38 -0300

    day 1 part 2

--- src/day1/day1_2.ts
+++ src/day1/day1_2.ts
@@ -2,13 +2,19 @@ import { readFileSync } from 'fs';
 
 console.time('executionTime');
 
+type similarity = {
+    numberToCompare: number;
+    timesItAppears: number;
+    rate: number;
+}
+
 const inputPath: string = __dirname + '/input.txt';
 const input = readFileSync(inputPath, 'utf-8');
 const groupOne: number[] = [];
 const groupTwo: number[] = [];
-const differences: number[] = [];
 const rows = input.split('\n');
 const rowsLength = rows.length;
+const similarities: similarity[] = [];
 
 rows.forEach((row) => {
     const splittedRow = row.split('   ');
@@ -22,12 +28,32 @@ rows.forEach((row) => {
 groupOne.sort((a: number, b: number) => a - b);
 groupTwo.sort((a: number, b: number) => a - b);
 
-for (let i = 0; i < rowsLength; i++) {
-    let absDiff = Math.abs(groupOne[i] - groupTwo[i]);
-    differences.push(absDiff);
-}
 
-const sum = differences.reduce((a, b) => a + b, 0);
+groupOne.forEach((num: number) => {
+    let timesItAppears = 0;
 
-console.log(`La suma de las diferencias es ${sum}`);
+    groupTwo.forEach((num2: number) => {
+        if (num === num2) {
+            timesItAppears++;
+        }
+    });
+
+    let match: similarity = {
+        numberToCompare: num,
+        timesItAppears: timesItAppears,
+        rate: num * timesItAppears
+    }
+
+    similarities.push(match);
+
+});
+
+let endScore: number = 0;
+similarities.forEach((similarity: similarity) => {
+
+    endScore += similarity.rate;
+});
+
+
+console.log(endScore);
 console.timeEnd('executionTime');
